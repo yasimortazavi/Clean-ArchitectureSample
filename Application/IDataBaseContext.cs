ce179578717fc4009671339086c1f47a25eb2aed
Yasi-Mortazavii <mortazaviiyasi@gmail.com>
2024-07-30 06:19:03 +0330

    Add project files.

---
+++ Application/IDataBaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public interface IDataBaseContext
+    {
+        DbSet<User> Users { get; set; }
+    }
+}
---
+++ Persistance/DataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Application;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance
+{
+    public class DataBaseContext:DbContext, IDataBaseContext
+    {
+        public DbSet<User> Users { get; set ; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=EfCoreCleanArc;User Id=DESKTOP-4PP4QV0;Password=admin123;Trusted_Connection=True;Integrated Security=True;TrustServerCertificate=True;");
+        }
+    }
+}
